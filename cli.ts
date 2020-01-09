213901ce41e9572c258d314e93fb178ac63a5e86
LucHighwalker <luc.highwalker@gmail.com>
2020-01-09 14:12:07 -0800

    Initializer creates base config

--- cli.ts
+++ cli.ts
@@ -1,40 +1,50 @@
 #!/usr/bin/env node
 
-import pkg from './package.json';
+import pkg from "./package.json";
+
+import Initializer from "./src/initializer";
+
+import * as path from "path";
 
 const [, , ...args] = process.argv;
 
 const main = async () => {
+  const isJS = path.basename(__filename)[4] == 'j';
   try {
-    const command = args[0] ? args[0] : '-h';
+    const command = args[0] ? args[0] : "-h";
 
     switch (command) {
-      case '-v':
-      case '--version':
+      case "-v":
+      case "--version":
         process.stdout.write(`Node On Rails version: ${pkg.version}\n`);
         break;
 
-      case '-h':
-      case '--help':
+      case "-h":
+      case "--help":
         // log help message
         break;
 
-      case '-n':
-      case '--new':
+      case "-n":
+      case "--new":
         const destination = process.cwd();
-        
+        const initializer = new Initializer("test", destination, isJS);
+        await initializer.initialize();
         break;
 
       default:
-        if (command[0] === '-') {
+        if (command[0] === "-") {
           throw new Error(
-            `Invalid command [${command}]. Use 'partum --help' for a list of commands.`,
+            `Invalid command [${command}]. Use 'partum --help' for a list of commands.`
           );
         }
     }
   } catch (error) {
     process.stdout.write(error);
   }
-}
+};
 
-main();
+try {
+  main();
+} catch (error) {
+  throw error;
+}
--- dist/cli.js
+++ dist/cli.js
@@ -11,33 +11,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const package_json_1 = __importDefault(require("./package.json"));
+const initializer_1 = __importDefault(require("./src/initializer"));
+const path = __importStar(require("path"));
 const [, , ...args] = process.argv;
-// const Generator = require('./src/generator');
-// const HelpMsg = require('./src/helpMsg');
-// const Initializer = require('./src/initializer');
-// const Options = require('./src/options');
 const main = () => __awaiter(this, void 0, void 0, function* () {
+    const isJS = path.basename(__filename)[4] == 'j';
     try {
-        const command = args[0] ? args[0] : '-h';
+        const command = args[0] ? args[0] : "-h";
         switch (command) {
-            case '-v':
-            case '--version':
+            case "-v":
+            case "--version":
                 process.stdout.write(`Node On Rails version: ${package_json_1.default.version}\n`);
                 break;
-            case '-h':
-            case '--help':
+            case "-h":
+            case "--help":
                 // log help message
                 break;
-            case '-i':
-            case '--init':
+            case "-n":
+            case "--new":
                 const destination = process.cwd();
-                const destArr = destination.split('/'); // eslint-disable-line
-                const name = destArr[destArr.length - 1];
+                const initializer = new initializer_1.default("test", destination, isJS);
+                yield initializer.initialize();
                 break;
             default:
-                if (command[0] === '-') {
+                if (command[0] === "-") {
                     throw new Error(`Invalid command [${command}]. Use 'partum --help' for a list of commands.`);
                 }
         }
@@ -46,4 +52,9 @@ const main = () => __awaiter(this, void 0, void 0, function* () {
         process.stdout.write(error);
     }
 });
-main();
+try {
+    main();
+}
+catch (error) {
+    throw error;
+}
